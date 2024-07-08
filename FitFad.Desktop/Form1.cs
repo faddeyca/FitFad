230c4571fa072183b0db4f2f5fd152ceed8699e9
Faddey Kryachko <faddeyca@gmail.com>
2024-07-08 17:59:30 +1200

    Add project files.

---
+++ FitFad.Desktop/Form1.cs
@@ -0,0 +1,10 @@
+namespace FitFad.Desktop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ FitFad.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace FitFad.Domain.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
